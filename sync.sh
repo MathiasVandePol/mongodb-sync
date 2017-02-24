c1462238d0f333d69fc9014573ece7bf75c746a0
Mathias Van de Pol <mathyke007@gmail.com>
2017-02-24 12:51:35 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-/backup.sh
-
-/restore.sh
+#/backup.sh
+#/restore.sh
+mongodump --out /backup/meteorback --host ${MONGODB_BACKUP_HOST} --port ${MONGODB_BACKUP_PORT} --db=${MONGODB_BACKUP_DB}
+if mongorestore --drop --host ${MONGODB_RESTORE_HOST} --port ${MONGODB_RESTORE_PORT} --db=${MONGODB_RESTORE_DB} /backup/meteorback; then
+    echo "   Restore succeeded"
+else
+    echo "   Restore failed"
+fi
